6f59eb016b0147a6f12b622bedec8d55fb875143
moonlitautumn <neb3la@gmail.com>
2024-12-18 20:49:48 -0300

    changed ost

--- build/scripts/soundon.js
+++ build/scripts/soundon.js
@@ -1,8 +1,8 @@
-var audio = document.getElementById('ambiance'); 
+var audio = document.getElementById('hmmmm'); 
 var hidee = document.getElementById('byebye'); 
 
 function unmuteambiance() {
-	if (audio.muted = true) {
+	if (audio.muted) {
     	audio.muted = false;
         hidee.innerHTML = "unmuted ^^";
 	} else {
