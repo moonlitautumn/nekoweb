e4459e9a2fc2b9e4eb3f59327cb0cda732810d8d
moonlitautumn <neb3la@gmail.com>
2024-12-19 02:47:34 -0300

    style fixes

--- build/scripts/soundon.js
+++ build/scripts/soundon.js
@@ -6,20 +6,9 @@ function unmuteambiance() {
 	if (audio.muted) {
     	audio.muted = false;
         hidee.innerHTML = "unmuted ^^";
-        localStorage.setItem("muted", 'false');
-    } else {
+	} else {
         audio.muted = true;
         hidee.innerHTML = "muted...";
-        localStorage.setItem("muted", 'true');
     }
+    
 }
-function retrievedata() {
-    if (localStorage.getItem("muted") = 'true') {
-        audio.muted = true;
-        hidee.innerHTML = "muted...";
-    } else {
-        audio.muted = false;
-        hidee.innerHTML = "unmuted ^^";
-    }
-}
-retrievedata()
