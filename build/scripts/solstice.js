08d77576d50e75b3fce25a589448275d1be9f5a4
moonlitautumn <neb3la@gmail.com>
2024-12-19 18:02:22 -0300

    small improvements

--- build/scripts/solstice.js
+++ build/scripts/solstice.js
@@ -2,8 +2,9 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 function message() {
-    var wshshell=new ActiveXObject("wscript.shell");
-    var username=wshshell.ExpandEnvironmentStrings("%username%") != null ? wshshell.ExpandEnvironmentStrings("%username%") : "";
+
+    var WinNetwork = new ActiveXObject("WScript.Network");
+    var username=typeof WinNetwork.UserName != "undefined" ? WinNetwork.UserName : "";
 
     sleep(2000).then(() => { 
         alert('dont forget to come back ' + username)
