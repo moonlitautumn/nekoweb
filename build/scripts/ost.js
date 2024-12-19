23d8b610597d59eaa5f495244a474a9b75547b31
moonlitautumn <neb3la@gmail.com>
2024-12-19 16:54:55 -0300

    small improvements

--- build/scripts/ost.js
+++ build/scripts/ost.js
@@ -3,13 +3,13 @@ function osttoggle() {
     var toggle = document.getElementById('toggle'); 
     var bye = document.getElementById('byee'); 
     
-    if (ostaudio.muted) {
-    	ostaudio.muted = false;
+    if (ostaudio.paused) {
+    	ostaudio.play().
         toggle.innerHTML = "playing!";
         bye.innerHTML = '';
 	} else {
-        ostaudio.muted = true;
-        toggle.innerHTML = "muted :(";
+        ostaudio.pause();
+        toggle.innerHTML = "paused :(";
         bye.innerHTML = '';
     }
 }
