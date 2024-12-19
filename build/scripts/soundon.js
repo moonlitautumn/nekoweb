409fe66854e720c60c52ccb87581a5e21130c12e
moonlitautumn <neb3la@gmail.com>
2024-12-18 21:08:49 -0300

    fixes

--- build/scripts/soundon.js
+++ build/scripts/soundon.js
@@ -1,7 +1,8 @@
-let audio = document.getElementById('plspls'); 
-var hidee = document.getElementById('byebye'); 
 
 function unmuteambiance() {
+    let audio = document.getElementById('plspls'); 
+    var hidee = document.getElementById('byebye'); 
+
 	if (audio.muted) {
     	audio.muted = false;
         hidee.innerHTML = "unmuted ^^";
