6f561fbb457dd0033c128d27defd81d4393ec45f
moonlitautumn <neb3la@gmail.com>
2024-12-19 18:18:39 -0300

    small improvements

--- build/scripts/solstice.js
+++ build/scripts/solstice.js
@@ -2,12 +2,15 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 function message() {
-
+    const soundeffect = document.getElementById('ping');
     sleep(2000).then(() => { 
+        soundeffect.play();
         alert('sorryyyy')
-        sleep(2000).then(() => { 
+        sleep(1000).then(() => { 
+            soundeffect.play()
             alert('this place is still work in progress..')
-            sleep(2000).then(() => { 
+            sleep(1000).then(() => { 
+                soundeffect.play()
                 alert('it will be done soon though so maybe check back later!')
                 
             });
