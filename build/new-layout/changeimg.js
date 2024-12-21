e74501260a62e3f321596a8a2bacc2072895f8d9
moonlitautumn <neb3la@gmail.com>
2024-12-20 21:07:29 -0300

    creating new layout

--- build/new-layout/changeimg.js
+++ build/new-layout/changeimg.js
@@ -1,11 +1,12 @@
 var frames = Array(
-    'https://sillyivy.nekoweb.org/new-layout/randompic.html', 15,
-    'https://dimden.neocities.org/navlink/', 15);
+    'https://sillyivy.nekoweb.org/new-layout/randompic.html', 10,
+    'https://dimden.neocities.org/navlink/', 10);
 var i = 0, len = frames.length;
 function ChangeSrc()
 {
   if (i >= len) { i = 0; } // start over
-  document.getElementById('imgrotate').src = frames[i++];
+  document.getElementById('imgimg').src = frames[i++];
+  console.log("change");
   setTimeout('ChangeSrc()', (frames[i++]*1000));
 }
-ChangeSrc();
+window.onload = ChangeSrc;
