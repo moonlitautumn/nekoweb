6d365749d41ac96bb5fb37c1ec1dcc3d0cab2e9d
moonlitautumn <neb3la@gmail.com>
2024-12-21 00:22:07 -0300

    creating new layout

--- build/new-layout/changeimg.js
+++ build/new-layout/changeimg.js
@@ -9,7 +9,7 @@ function move() {
     j = 1;
     var elem = document.getElementById("progress");
     var width = 1;
-    var id = setInterval(frame, 3000);
+    var id = setInterval(frame, 333);
     function frame() {
       if (width >= 100) {
         clearInterval(id);
