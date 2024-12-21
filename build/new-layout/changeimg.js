1c3b6714c13db6803fe403beda23548baeb8857d
moonlitautumn <neb3la@gmail.com>
2024-12-21 00:12:49 -0300

    creating new layout

--- build/new-layout/changeimg.js
+++ build/new-layout/changeimg.js
@@ -2,33 +2,33 @@ var frames = Array(
     'https://sillyivy.nekoweb.org/new-layout/randompic.html', 30,
     'https://dimden.neocities.org/navlink/', 30);
 var i = 0, len = frames.length;
-function ChangeSrc()
-{
-  if (i >= len) { i = 0; } // start over
-  document.getElementById('imgimg').src = frames[i++];
-  console.log("change");
-  document.getElementById('progress').style.width = '0%';
-  clearInterval(id);
-  a = 0;
-  move();
-  setTimeout('ChangeSrc()', (frames[i++]*1000));
-}
-var a = 0;
+
+var j = 0;
 function move() {
-  if (a == 0) {
-    a = 1;
+  if (j == 0) {
+    j = 1;
     var elem = document.getElementById("progress");
-    var width = 0;
-    var id = setInterval(frame2, 3.33333333);
-    function frame2() {
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
       if (width >= 100) {
         clearInterval(id);
-        a = 0;
+        j = 0;
       } else {
         width++;
         elem.style.width = width + "%";
       }
     }
   }
-} 
+}
+function ChangeSrc()
+{
+  if (i >= len) { i = 0; } // start over
+  document.getElementById('imgimg').src = frames[i++];
+  console.log("change");
+  document.getElementById('progress').style.width = '0%';
+  move();  
+  setTimeout('ChangeSrc()', (frames[i++]*1000));
+
+}
 window.onload = ChangeSrc;
