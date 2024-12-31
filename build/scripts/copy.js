3cd4f76025e13273311b5515b9d488075a8769a8
moonlitautumn <neb3la@gmail.com>
2024-12-31 03:12:53 -0300

    new artt

--- build/scripts/copy.js
+++ build/scripts/copy.js
@@ -5,3 +5,10 @@ function copytext() {
     soundeffect.play();
     alert("copied to clipboard! (tysm!!)")
 } 
+function copytext2() {
+    const copyText = '85e2Jw3zKrXUaE2Zz3CeyCX1MLC1PYGD5jH3fHVWQCFsbimVDRyQs7LGwT2yzLtAxGEF7F6F7thWad7Liyknunn955f5oLq';
+    navigator.clipboard.writeText(copyText);
+    let soundeffect = document.getElementById('ping');
+    soundeffect.play();
+    alert("I LOVE YOU SO MUCH AAA")
+} 
