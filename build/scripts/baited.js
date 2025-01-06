175785355ef83f32912b742d2ece7428721cf6f7
moonlitautumn <neb3la@gmail.com>
2025-01-06 05:53:30 -0300

    idk

--- build/scripts/baited.js
+++
@@ -1,7 +0,0 @@
-function test() {
-    let isperv = localStorage.getItem("baited");
-    if (isperv == "true") {
-        document.getElementById("test").innerHTML = "baited = TRUE!!"
-    } 
-}
-test()
