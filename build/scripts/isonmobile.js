14884f65e9525fdee9cb15a56d10a3138cf3c64f
moonlitautumn <neb3la@gmail.com>
2025-01-06 22:35:08 -0300

    pls work vid

--- build/scripts/isonmobile.js
+++ build/scripts/isonmobile.js
@@ -8,15 +8,20 @@ let regexp = /android|iphone/i;
 /* Using test() method to search regexp in details 
 it returns boolean value*/
 let isMobileDevice = regexp.test(details); 
+const params = new URLSearchParams(window.location.search);
+let reset = params.has("reset-device");
   
 if (isMobileDevice) { 
     console.log("Mobile Device"); 
-
-    if (localStorage.getItem("notified") == "true") {
-        console.log("already alerted");
+    if (reset == true) {
+        localStorage.removeItem("notified");
     } else {
-        alert("this website was designed for pc's / wide displays, it will prob look bad on your device!!");
-        localStorage.setItem("notified", "true");
+        if (localStorage.getItem("notified") == "true") {
+            console.log("already alerted");
+        } else {
+            alert("this website was designed for pc's / wide displays, it will prob look bad on your device!!");
+            localStorage.setItem("notified", "true");
+        }
     }
 } else { 
     console.log("Desktop"); 
