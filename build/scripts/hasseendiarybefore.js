1423b77e644d06a428c48d28905d00245f9137c5
moonlitautumn <neb3la@gmail.com>
2025-01-08 05:27:50 -0300

    aaa

---
+++ build/scripts/hasseendiarybefore.js
@@ -0,0 +1,17 @@
+const params1 = new URLSearchParams(window.location.search);
+let reset1 = params1.has("reset-diary");
+  
+
+ if (reset1) {
+    localStorage.removeItem("seen-diary");
+    console.log("reset diary");
+    alert("diary reset!!")
+} else {
+    console.log(reset1);
+    if (localStorage.getItem("seen-diary") == "true") {
+        console.log("silence");
+    } else {
+        document.getElementById('music').src = "";
+        localStorage.setItem("seen-diary", "true");
+    }
+}
