f891aebc4e767429745aa1aba944f058f9f8c6ef
moonlitautumn <neb3la@gmail.com>
2025-01-08 05:47:05 -0300

    aaa

--- build/scripts/hasseendiarybefore.js
+++ build/scripts/hasseendiarybefore.js
@@ -11,7 +11,7 @@ let reset1 = params1.has("reset-diary");
     if (localStorage.getItem("seen-diary") == "true") {
         console.log("silence");
         document.getElementById('music').muted = "true";
-        document.getElementById('change').innerHTML = "visiting again? ^^ (tyyy!)";
+        document.getElementById('change').innerHTML = "welcome back to whitespace";
         document.getElementById('change').style.color = "#FF4B47";
     } else {
         localStorage.setItem("seen-diary", "true");
