85921316ef858a2b88a9ea98f465dd066b0e9ab3
moonlitautumn <neb3la@gmail.com>
2025-01-13 02:25:41 -0300

    added plasmatrap!

--- build/scripts/sendmenotif.js
+++ build/scripts/sendmenotif.js
@@ -1,3 +1,4 @@
-document.getElementById("sendmsg").onclick=function(){
+function send() {
     let txt=document.getElementById("send-msg-box");
-    txt.value!=""&&(fetch("https://ntfy.plasmatrap.com/benjae",{method:"POST",body:txt.value}),txt.value="sent!")};
+    txt.value!=""&&(fetch("https://ntfy.plasmatrap.com/sillyivyluna",{method:"POST",body:txt.value}),txt.value="")
+};
