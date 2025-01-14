69d39f5ecda843111180c8f722e9f8544f3c5958
moonlitautumn <neb3la@gmail.com>
2025-01-13 22:13:30 -0300

    fixed some of my sleepybrains mistakes

--- build/scripts/sendmenotif.js
+++ build/scripts/sendmenotif.js
@@ -5,7 +5,7 @@ function send() {
     let txt=document.getElementById("send-msg-box");
     txt.value!=""&&(fetch("https://ntfy.plasmatrap.com/sillyivyluna",{method:"POST",body:txt.value}),txt.value="")
     let soundeffect = document.getElementById('ping');
-    sleep(150);
+    sleep(500);
     soundeffect.play();
     alert("message sent!");
 };
